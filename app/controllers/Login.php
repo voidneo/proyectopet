7dbc3fdddec8ff5c73c0f4a096fe722836725995
Neo.h <nicolas.mg.uy@gmail.com>
2022-05-20 00:25:01 -0300

    Minor format fix.

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -5,17 +5,16 @@ class Login extends Controller {
     protected $view;
 
     public function index($data = []) {
-        if(!isset($_SESSION['token'])) {
-
-            $_SESSION['security_hash'] = hash('sha1', random_bytes(8));
-
-            $data["title"] = "Polo tecnologico - Iniciar sesion";
-            $data["hash"] = $_SESSION['security_hash'];
-
-            $this->load_view("LoginPage", $data);
-        } else {
+        if(isset($_SESSION['token'])) {
             header("Location: ./");
         }
+
+        $_SESSION['security_hash'] = hash('sha1', random_bytes(8));
+
+        $data["title"] = "Iniciar sesion";
+        $data["hash"] = $_SESSION['security_hash'];
+
+        $this->load_view("LoginPage", $data);
     }
 }
 
