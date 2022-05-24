6887375f948a7bf7a8c94e5d642a9bf907d16d84
Neo.h <nicolas.mg.uy@gmail.com>
2022-05-24 15:34:42 -0300

    Removed obsolete test code. Renamed input field names and IDs.

--- public/scripts/login-form.js
+++ public/scripts/login-form.js
@@ -1,22 +1,10 @@
-window.T = async () => {
-	formData = new FormData();
-	formData.append("user", document.getElementById("user").value);
-	formData.append("pass", document.getElementById("pass").value);
-	formData.append("hash", document.getElementById("hash").value);
-
-	return await fetch("./api/authenticate", {
-		method: "POST",
-		body: formData,
-	});
-};
-
 document.getElementById("form").addEventListener("submit", (evt) => {
 	evt.preventDefault();
 
 	formData = new FormData();
-	formData.append("user", document.getElementById("user").value);
-	formData.append("pass", document.getElementById("pass").value);
-	formData.append("hash", document.getElementById("hash").value);
+	formData.append("cedula", document.getElementById("cedula").value);
+	formData.append("contrasena", document.getElementById("contrasena").value);
+	formData.append("security_hash", document.getElementById("security_hash").value);
 
 	fetch("./api/authenticate", {
 		method: "POST",
