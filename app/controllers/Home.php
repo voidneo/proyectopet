c225d5ebbcd7d87ecb4869a3616554772fe58446
Neo.h <nicolas.mg.uy@gmail.com>
2022-05-24 15:40:29 -0300

    Changed mockup model for real model with real data from the database.

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,9 +4,14 @@ class Home extends Controller {
 
     public function index($data = []) {
         if(isset($_SESSION["token"])) {
+            $this->load_model("Usuario");
+            $user = new Usuario;
+            $user = $user->findByCI($_SESSION["cedula"]);
+
             $data["token"] = $_SESSION["token"];
-            $data["user"] = $this->load_model("User");
-            $data["user"]->load("");
+            $data["cedula"] = $user->getCI();
+            $data["nombre"] = $user->getNombre();
+            $data["apellido"] = $user->getApellido();
         }
 
         $this->load_view("HomePage", $data);
