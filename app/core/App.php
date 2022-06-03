2282922797052168c422520c46fb54683fe64a7a
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-02 22:38:43 -0300

    Moved routing logic into its own class.

--- app/core/App.php
+++ app/core/App.php
@@ -1,36 +1,11 @@
 <?php
 
-class App {
-
-    protected $controller = "home";
-    protected $method = "index";
-    protected $arguments = [];
+class App extends Controller {
 
     public function __construct() {
-        $url = $this->parse_url();
-
-        if(isset($url[0]) && file_exists("../app/controllers/" . $url[0] . ".php")) {
-            $this->controller = $url[0];
-            unset($url[0]);
-        }
-
-        require_once("../app/controllers/" . $this->controller . ".php");
-        $this->controller = new $this->controller;
-
-        if(isset($url[1]) && method_exists($this->controller, $url[1])) {
-            $this->method = $url[1];
-            unset($url[1]);
-        }
-
-        $this->arguments = $url ? array_values($url) : [];
-
-        call_user_func_array([$this->controller, $this->method], [$this->arguments]);
-
-        /*try {
-            call_user_func_array([$this->controller, $this->method], [$this->arguments]);
-        } catch(TypeError $err) {
-            $this->controller->home([$method, $arguments]);
-        }*/
+        $this->load_controller("Router");
+        $router = new Router;
+        $router->route($this->parse_url());
     }
 
     public function parse_url() {
