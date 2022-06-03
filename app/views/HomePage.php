2f4a1e6316f3f34e59ec055451d51c4043a45f20
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-02 22:57:25 -0300

    Homepage now greets users differently depending on if they're logged in or not.

--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -1,10 +1,24 @@
-<h3>Homepage</h3>
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-if(isset($data["token"])) {
-    echo "<h4>Bienvenido " . $data["nombre"] . " " . $data["apellido"] . "</h4>";
-} else {
-    echo "<h4>Bienvenido usuario</h4>";
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="text/javascript" src="./scripts/jeiQuery-100-real-no-fake.js"></script>
+</head>
 
-?>
+<body>
+    <h3 id="a">Homepage</h3>
+    <?php
+
+    if (isset($data["token"])) {
+        echo "<h4>Bienvenido " . $data["nombre"] . " " . $data["apellido"] . "</h4>";
+    } else {
+        echo "<h4>Bienvenido usuario</h4>";
+    }
+
+    ?>
+</body>
+</html>
