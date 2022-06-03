a7f7912901ae7339a8068d7f12c045f95ba50404
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-02 22:40:29 -0300

    Modified load script to search for views/controllers inside subfolders too.

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,13 +2,33 @@
 
 class Controller {
 
+    private static function resolvePath($path) {
+        $folders    = explode("/", $path);
+        $nOfFolders = count($folders) - 1;
+        $script      = $folders[$nOfFolders];
+        $path = "";
+
+        for($i = 0; $i < $nOfFolders; $i++) {
+            $path = $path . "$folders[$i]/";
+        }
+
+        return ["path" => $path, "name" => $script];
+    }
+
+    public function index() {}
+
     public function load_model($model) {
         require_once("../app/models/" . $model . ".php");
-        return new $model;
     }
 
     public function load_view($view, $data = []) {
-        require_once("../app/views/" . $view . ".php");
+        $script = self::resolvePath($view);
+        require_once("../app/views/" . $script["path"] . $script["name"] . ".php");
+    }
+
+    public function load_controller($ctrl) {
+        $script = self::resolvePath($ctrl);
+        require_once("../app/controllers/" . $script["path"] . $script["name"] . ".php");
     }
 
 }
