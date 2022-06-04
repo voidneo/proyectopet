d390fa4475c7de0d137710302319f0e70817817e
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-03 22:05:11 -0300

    Created interactive view to display and manage categories.

---
+++ app/views/components/CategoryList.php
@@ -0,0 +1,25 @@
+<table id="category-widget">
+    <thead>
+        <tr>
+            <td colspan="3">
+                <input type="text" id="search-box" placeholder="Buscar.." />
+                <button id="search-btn">Buscar</button>
+            </td>
+        </tr>
+        <tr>
+            <td>ID</td>
+            <td>Nombre</td>
+            <td>Opciones</td>
+        </tr>
+    </thead>
+    <tbody id="table-body">
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="3">
+                <span id="pages"></span>
+            </td>
+        </tr>
+    </tfoot>
+</table>
+<script type="text/javascript" src="./scripts/category-list.js"></script>
