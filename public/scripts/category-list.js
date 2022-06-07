9a44f1fe69b350ddadcce0aecb7bb06a93fb8603
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-07 20:26:29 -0300

    Created functions to create, update and delete categories. Refactored some of the code. Now the view gets updated when a setting is changed (page, results per page, update, creation and deletion). Implemented security hash.

--- public/scripts/category-list.js
+++ public/scripts/category-list.js
@@ -1,118 +1,297 @@
-async function getCategories(pagination = {"page": 1, "rows_per_page": 5}, order = {"column": "id", "order": "ASC"}) {
-    url = new URL("http://localhost/proyectopet/public/api/category/read");
-    a = JSON.stringify(pagination);
-    b = JSON.stringify(order);
-    url.searchParams.append("pagination", a);
-    url.searchParams.append("order", b);
+// TODO: implement search by value
+// TODO: load same page number when refreshing the table after creating a category
 
-    return await fetch(url, { method: "GET" });
+async function getCategories(
+	pagination = { page: 1, rows_per_page: 5 },
+	order = { column: "id", order: "ASC" }
+) {
+    let securityHashInput = document.querySelector("#security-hash");
+    // TODO: find workaround url object
+	url = new URL("http://localhost/proyectopet/public/api/category/read");
+	a = JSON.stringify(pagination);
+	b = JSON.stringify(order);
+    c = securityHashInput.value;
+	url.searchParams.append("pagination", a);
+	url.searchParams.append("order", b);
+	url.searchParams.append("security_hash", c);
+
+	return await fetch(url, { method: "GET" });
+}
+
+function createCategory(name) {
+    let pageLengthCtrl     = document.querySelector("#rows-per-page");
+	let orderColumnCtrl    = document.querySelector("#order-column");
+	let orderDirectionCtrl = document.querySelector("#order-direction");
+    let securityHashInput  = document.querySelector("#security-hash");
+
+	formData = new FormData();
+	formData.append("nombre", name);
+    formData.append("security_hash", securityHashInput.value);
+
+	fetch("./api/category/create", {
+		method: "POST",
+		body: formData,
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data["error"]) {
+				console.log(data["error"]);
+			} else {
+                getCategories(
+                    { page: 1, rows_per_page: pageLengthCtrl.value },
+                    { column: orderColumnCtrl.value, order: orderDirectionCtrl.value }
+                )
+                    .then((response) => response.json())
+                    .then(refreshTable)
+                    .catch(console.log);
+            }
+		})
+		.catch(console.log);
+}
+
+function updateCategory(id, name) {
+	let input              = document.querySelector("#id-" + id);
+    let securityHashInput  = document.querySelector("#security-hash");
+    
+	formData = new FormData();
+	formData.append("id", id);
+	formData.append("nombre", name);
+    formData.append("security_hash", securityHashInput.value);
+
+	fetch("./api/category/update", {
+		method: "POST",
+		body: formData,
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data["error"]) {
+				console.log(data["error"]);
+				input.value = input.getAttribute("previous-value");
+			}
+		})
+		.catch((err) => {
+			input.value = input.getAttribute("previous-value");
+		});
+}
+
+function deleteCategory(id) {
+    let securityHashInput  = document.querySelector("#security-hash");
+
+	formData = new FormData();
+	formData.append("id", id);
+    formData.append("security_hash", securityHashInput.value);
+
+	fetch("./api/category/delete", {
+		method: "POST",
+		body: formData,
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data["error"]) {
+				console.log(data["error"]);
+			} else {
+				let table = document.querySelector("#table-body");
+				let row = document.querySelector("#row-" + id);
+				table.removeChild(row);
+			}
+		})
+		.catch(console.error);
+}
+
+function removeAllChildren(id) {
+	let parent = document.querySelector("#" + id);
+	for (i = parent.children.length - 1; parent.children.length > 0; i--) {
+		parent.children[i].remove();
+	}
 }
 
 function generatePagination(results) {
-    let currentPage  = results["content"]["page"];
-    const rows       = results["content"]["rows"];
-    const total_rows = results["content"]["total_rows"];
-    const nOfPages   = rows == 0 ? total_rows / 5 : total_rows / rows;
-    
-    let pages        = document.querySelector("#pages");
-    pages.innerHTML  = "";
+	const rows_per_page      = document.querySelector("#rows-per-page").value;
+	const orderColumnCtrl    = document.querySelector("#order-column");
+	const orderDirectionCtrl = document.querySelector("#order-direction");
 
-    for(let i = 1; i <= nOfPages; i++) {
-        let btn = document.createElement("button");
-        btn.setAttribute("id", "page-" + i);
-        
-        if(currentPage == i)
-            btn.setAttribute("disabled", "");
+	const currentPage = results["content"]["page"];
+	const rows        = results["content"]["rows"];
+	const total_rows  = results["content"]["total_rows"];
+	const nOfPages    = Math.ceil(total_rows / rows_per_page);
 
-        btn.innerText = i;
-        pages.appendChild(btn);
-        pages.innerHTML += " ";
-    }
+	let pages = document.querySelector("#pages");
 
-    for(let i = 1; i <= nOfPages; i++) {
-        let btn = document.querySelector("#page-" + i);
-        document.querySelector("#page-" + i).addEventListener("click", evt => {
-            getCategories(
-                {"page": i, "rows_per_page": 5} // TODO: get rows per page from html controller
-            )
-            .then(res => res.json())
-            .then(data => {
-                for(let i = 1; i <= nOfPages; i++) {
-                    let btn = document.querySelector("#page-" + i);
-                    btn.remove();
-                }
-                populateTable(data);
-                generatePagination(data);
-            })
-            .catch(console.error);
-        });
-    }
+	for (let i = 1; i <= nOfPages; i++) {
+		let btn = document.createElement("button");
+		btn.setAttribute("id", "page-" + i);
+
+		if (currentPage == i) btn.setAttribute("disabled", "");
+
+		btn.innerText = i;
+		pages.appendChild(btn);
+		pages.innerHTML += " ";
+	}
+
+	for (let i = 1; i <= nOfPages; i++) {
+		let btn = document.querySelector("#page-" + i);
+		document.querySelector("#page-" + i).addEventListener("click", (evt) => {
+			let rowsPerPage = document.querySelector("#rows-per-page").value;
+			getCategories(
+				{ page: i, rows_per_page: rowsPerPage },
+				{ column: orderColumnCtrl.value, order: orderDirectionCtrl.value }
+			)
+				.then((response) => response.json())
+				.then(refreshTable)
+				.catch(console.log);
+		});
+	}
 }
 
 function populateTable(results) {
-    let table = document.querySelector("#table-body");
-    table.innerHTML = "";
+	let table = document.querySelector("#table-body");
 
-    rows = results["content"]["results"];
-    for(i = 0; i < rows.length; i++) {
-        let tr = document.createElement("tr");
+	rows = results["content"]["results"];
+	for (i = 0; i < rows.length; i++) {
+		let id    = rows[i]["id"];
+		let name  = rows[i]["nombre"];
 
-        let c1 = document.createElement("td");
-        c1.innerText = rows[i]["id"];
-        tr.appendChild(c1);
-
-        let input = document.createElement("input");
-        input.setAttribute("value", rows[i]["nombre"]);
-        input.setAttribute("id", "id-" + rows[i]["id"]);
-        input.setAttribute("type", "text");
-        input.setAttribute("readonly", "true");
-
-        let c2 = document.createElement("td");
-        c2.appendChild(input);
-        tr.appendChild(c2);
-
-        let edit = document.createElement("a");
-        edit.innerText = "Editar";
-        edit.setAttribute("href", "#");
-        // TODO: add evt listener
+		let tr    = document.createElement("tr");
+		let c1    = document.createElement("td");
+		let input = document.createElement("input");
+		let c2    = document.createElement("td");
+		let edit  = document.createElement("a");
+		let del   = document.createElement("a");
+		let c3    = document.createElement("td");
         
-        let del = document.createElement("a");
-        del.innerText = "Eliminar";
-        del.setAttribute("href", "#")
-        
-        let c3 = document.createElement("td");
-        c3.appendChild(edit);
-        c3.innerHTML += " | ";
-        c3.appendChild(del);
-        tr.appendChild(c3);
+		tr.setAttribute("id", "row-" + id);
 
-        table.appendChild(tr);
+		c1.innerText = id;
+		tr.appendChild(c1);
 
-        del.addEventListener("click", evt => {
-            // TODO: prompt to confirm
-            // TODO: delete through API
-            table.removeChild(tr);
-        });
+		input.setAttribute("value", name);
+		input.setAttribute("id", "id-" + id);
+		input.setAttribute("type", "text");
+		input.setAttribute("readonly", "true");
 
-        input.addEventListener("dblclick", evt => {
-            input.removeAttribute("readonly");
-        });
+		c2.appendChild(input);
+		tr.appendChild(c2);
 
-        input.addEventListener("keyup", evt => {
-            if(evt.code == "Enter") {
-                input.setAttribute("readonly", "true");
-                // TODO: update through API
-            }
-        });
-    }
+		edit.innerText = "Editar";
+		edit.setAttribute("href", "#");
+
+		del.innerText = "Eliminar";
+		del.setAttribute("href", "#");
+
+		c3.appendChild(edit);
+		c3.innerHTML += " | ";
+		c3.appendChild(del);
+		tr.appendChild(c3);
+
+		table.appendChild(tr);
+
+		edit.addEventListener("click", (evt) => {
+			// FIXME: this event doesn't fire because javascript, why else
+			input.removeAttribute("readonly");
+			input.focus();
+		});
+
+		del.addEventListener("click", (evt) => {
+			if (confirm("Seguro que desea eliminar el registro '" + name + "'?")) {
+				deleteCategory(id);
+			}
+		});
+
+		input.addEventListener("dblclick", (evt) => {
+			input.setAttribute("previous-value", input.value);
+			input.removeAttribute("readonly");
+		});
+
+		input.addEventListener("keyup", (evt) => {
+			if (evt.code == "Enter") {
+                // TODO: validate the data
+				input.setAttribute("readonly", "true");
+				updateCategory(id, input.value);
+			} else if (evt.code == "Escape") {
+				input.value = input.getAttribute("previous-value");
+				input.setAttribute("readonly", "true");
+			}
+		});
+	}
 }
 
-window.addEventListener("load", evt => {
-    getCategories()
-    .then(response => response.json())
-    .then(data => {
-        populateTable(data);
-        generatePagination(data);
+function refreshTable(data) {
+	removeAllChildren("table-body");
+	populateTable(data);
+	removeAllChildren("pages");
+	generatePagination(data);
+}
+
+window.addEventListener("load", (evt) => {
+	let pageLengthCtrl     = document.querySelector("#rows-per-page");
+	let idTableHeader      = document.querySelector("#column-id");
+	let nameTableHeader    = document.querySelector("#column-nombre");
+	let orderColumnCtrl    = document.querySelector("#order-column");
+	let orderDirectionCtrl = document.querySelector("#order-direction");
+	let catNameInput       = document.querySelector("#cat-creation-name");
+	let catCreationBtn     = document.querySelector("#cat-creation-btn");
+
+	idTableHeader.addEventListener("click", (evt) => {
+		if (orderColumnCtrl.value == "id") {
+			orderDirectionCtrl.value =
+				orderDirectionCtrl.value == "asc" ? "desc" : "asc";
+		} else {
+			orderColumnCtrl.value = "id";
+			orderDirectionCtrl.value = "asc";
+		}
+
+		getCategories(
+			{ page: 1, rows_per_page: pageLengthCtrl.value },
+			{ column: orderColumnCtrl.value, order: orderDirectionCtrl.value }
+		)
+			.then((response) => response.json())
+			.then(refreshTable)
+			.catch(console.log);
+	});
+
+	nameTableHeader.addEventListener("click", (evt) => {
+		if (orderColumnCtrl.value == "nombre") {
+			orderDirectionCtrl.value =
+				orderDirectionCtrl.value == "asc" ? "desc" : "asc";
+		} else {
+			orderColumnCtrl.value = "nombre";
+			orderDirectionCtrl.value = "asc";
+		}
+
+		getCategories(
+			{ page: 1, rows_per_page: pageLengthCtrl.value },
+			{ column: orderColumnCtrl.value, order: orderDirectionCtrl.value }
+		)
+			.then((response) => response.json())
+			.then(refreshTable)
+			.catch(console.log);
+	});
+
+	nameTableHeader.addEventListener("click", (evt) => {
+		document.querySelector("#order-column").value = "nombre";
+	});
+
+	pageLengthCtrl.addEventListener("change", (evt) => {
+		getCategories(
+			{ page: 1, rows_per_page: pageLengthCtrl.value },
+			{ column: orderColumnCtrl.value, order: orderDirectionCtrl.value }
+		)
+			.then((response) => response.json())
+			.then(refreshTable)
+			.catch(console.log);
+	});
+
+    catCreationBtn.addEventListener("click", evt => {
+        if(catNameInput.value != "") {
+            createCategory(catNameInput.value);
+        }
     })
-    .catch(console.log);
+
+	getCategories(
+		{ page: 1, rows_per_page: pageLengthCtrl.value },
+		{ column: orderColumnCtrl.value, order: orderDirectionCtrl.value }
+	)
+		.then((response) => response.json())
+		.then(refreshTable)
+		.catch(console.log);
 });
