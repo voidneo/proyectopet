9b9599b5aa345a075e70f12e5164255d22a2e31e
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-07 20:17:13 -0300

    Added elements to control how many results per page, security hash, to sort and create new categories.

--- app/views/components/CategoryList.php
+++ app/views/components/CategoryList.php
@@ -4,17 +4,34 @@
             <td colspan="3">
                 <input type="text" id="search-box" placeholder="Buscar.." />
                 <button id="search-btn">Buscar</button>
+                <select id="rows-per-page">
+                    <option>5</option>
+                    <option>10</option>
+                    <option>15</option>
+                    <option>20</option>
+                    <option>25</option>
+                    <option>30</option>
+                </select>
             </td>
+            <input type="hidden" id="order-column" value="id"/>
+            <input type="hidden" id="order-direction" value="asc"/>
         </tr>
         <tr>
-            <td>ID</td>
-            <td>Nombre</td>
+            <td id="column-id">ID</td>
+            <td id="column-nombre">Nombre</td>
             <td>Opciones</td>
         </tr>
     </thead>
     <tbody id="table-body">
     </tbody>
     <tfoot>
+        <tr>
+            <td colspan="3">
+                <input type="text" id="cat-creation-name" placeholder="Categoria..." />
+                <input type="hidden" id="security-hash" value="<?php echo $data["security_hash"] ?>" />
+                <input type="submit" id="cat-creation-btn" value="Crear" />
+            </td>
+        </tr>
         <tr>
             <td colspan="3">
                 <span id="pages"></span>
