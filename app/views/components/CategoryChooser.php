4ac004dd5caea8ecee6163f5f48ecafa2b70c9ab
Neo.h <nicolas.mg.uy@gmail.com>
2022-06-16 20:09:04 -0300

    Created component to load categories from db and display them as suggestion as the user types in.

---
+++ app/views/components/CategoryChooser.php
@@ -0,0 +1,4 @@
+<style>input[valid=false] { color:red; } input[valid=true] { color:green; }</style>
+<input list="cat-list" name="category" id="cat-chooser" placeholder="Categoria...">
+  <datalist id="cat-list"></datalist>
+<script type="text/javascript" src="./scripts/category-chooser.js"></script>
